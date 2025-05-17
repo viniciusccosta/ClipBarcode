c8f182071ff6f316c9169d21b42576927a6382c9
viniciusccosta <viniciusccosta95@gmail.com>
2025-05-17 10:27:35 -0300

    Add constants for history path, current version, label font, and Tesseract default paths

---
+++ clipbarcode/constants.py
@@ -0,0 +1,15 @@
+import toml
+from packaging import version
+
+from clipbarcode.utils import resource_path
+
+HISTORY_PATH = resource_path("./history")
+CUR_VERSION = version.parse(
+    toml.load(resource_path("pyproject.toml"))["tool"]["poetry"]["version"]
+)
+
+LABEL_FONTNAME = "Arial"
+TESSERACT_DEFAULT_PATHS = {
+    "posix": "/usr/bin/tesseract",
+    "nt": "C:\\Program Files\\Tesseract-OCR\\tesseract.exe",
+}
