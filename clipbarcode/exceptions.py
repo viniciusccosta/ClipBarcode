144f813c45ecc36af501a15b8e075338ef702123
viniciusccosta <viniciusccosta95@gmail.com>
2025-05-18 12:42:54 -0300

    Add DuplicatedLeituraException class to handle duplicate leitura errors

--- clipbarcode/exceptions.py
+++ clipbarcode/exceptions.py
@@ -5,3 +5,9 @@ class NoImageException(Exception):
 class LeituraFalhaException(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class DuplicatedLeituraException(Exception):
+    def __init__(self, message, leitura=None):
+        self.message = message
+        self.leitura = leitura
